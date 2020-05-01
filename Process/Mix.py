a28cfbb3a9ee050237d1bd6660c6c9562c037a31
HenryZeng <z13405868569@163.com>
2020-05-01 23:11:11 +0800

    差不多快好了

--- Process/Mix.py
+++ Process/Mix.py
@@ -2,6 +2,11 @@ from .imports import *
 from .window_about import window_about
 from .window_loading import window_loading
 from .window_main import window_main
+from .window_graphics import window_graphics
+
+
+class window_graphics_(window_graphics):
+    pass
 
 
 class window_loading_(window_loading):
@@ -19,13 +24,23 @@ class window_main_(window_main):
     def About_Window(self):
         about.show()
 
+    def Play_selected(self):
+        super().selected()
+        for select in self.selected:
+            graphics[select] = window_graphics_(config=self.configs[select], root=self.pkg_root[select])
+            graphics[select].show()
 
+
+debug = 2
 app = QtWidgets.QApplication(sys.argv)
 
 main = window_main_()
 about = window_about_()
-
+graphics = {}
 loading = window_loading_()
-loading.show()
-
+# 加载各个窗体
+if debug == 1:
+    loading.show()
+elif debug == 2:
+    main.show()
 sys.exit(app.exec_())
