f95258bbeb7099e474eaa689ee114670e2783afb
Henry Zeng <z13405868569@163.com>
2020-08-04 13:21:23 +0800

    增加对PushMsg的支持

--- TrayIcon.py
+++ TrayIcon.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 from PySide2 import QtGui, QtWidgets
-
+from DataUnCopy import Add,Space
 
 class TrayIcon(QtWidgets.QSystemTrayIcon):
     def __init__(self, parent=None):
@@ -12,6 +12,7 @@ class TrayIcon(QtWidgets.QSystemTrayIcon):
         self.popMenu.setVisible(False)
 
         self.activated.connect(self.Click)
+        Space["CoreControl"].MsgPush.connect(self.MsgPush)
 
     def AddActions(self, name, job):
         self.MenuActions[name] = QtWidgets.QAction(name, self)
@@ -21,3 +22,5 @@ class TrayIcon(QtWidgets.QSystemTrayIcon):
     def Click(self):
         self.popMenu.move(QtGui.QCursor.pos())
         self.popMenu.setVisible(True)
+    def MsgPush(self,tip,msg):
+        self.showMessage(tip, msg, self.icon())
