764dbc719815a6eb30981459abc28f01a9b3ca34
Henry Zeng <z13405868569@163.com>
2020-08-17 22:35:46 +0800

    人物移动事件支持

---
+++ Process/PersonMove.py
@@ -0,0 +1,33 @@
+from PySide2 import QtCore
+from DataUnCopy import Space
+import time
+class Move(QtCore.QThread):
+    def __init__(self):
+        super().__init__()
+        self.x_add = 0
+        self.y_add = 0
+        self.time_ = 0
+
+    def run(self):
+        Unit_add_x = self.x_add/self.time_
+        Unit_add_y = self.y_add / self.time_
+        Start = time.time()
+        X = Space['Info']["Move"]["Window"]['PersonX']
+        Y = Space['Info']["Move"]["Window"]['PersonY']
+        Go_pass = 0.0
+        while(Go_pass<=self.time_):
+            Space['Info']["Move"]["Window"]['PersonX'] = X+Unit_add_x*Go_pass
+            Space['Info']["Move"]["Window"]['PersonY'] = Y+Unit_add_y*Go_pass
+            Space["CoreControl"].Move.emit()
+            Go_pass = time.time() - Start
+            time.sleep(0.01)
+
+    def Move(self,Action):
+        if self.isRunning():
+            self.terminate()
+
+        self.x_add = Action["x_add"]
+        self.y_add = Action["y_add"]
+        self.time_ = Action["time"]
+
+        self.start()
