ee7e4a838fdf4d4eb5d68077cd914d6e7e71fde2
HenryZeng <zengdeveloper@qq.com>
2023-11-07 19:54:20 +0800

    完成初始界面

---
+++ t.py
@@ -0,0 +1,20 @@
+class a():
+    def run(self):
+        x.xx()
+
+
+
+
+class b():
+
+    def run(self):
+        print(1)
+        xx = a()
+        xx.run()
+
+    def xx(self):
+        print(2)
+
+if __name__ == "__main__":
+    x = b()
+    x.run()
